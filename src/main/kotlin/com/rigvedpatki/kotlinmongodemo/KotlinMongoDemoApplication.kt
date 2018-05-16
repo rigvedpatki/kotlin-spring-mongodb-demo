ae62e5c983d75b22e703ae2d86fe847138191318
Rigved Patki <rigved.patki@gmail.com>
2018-05-16 15:38:42 +0530

    initial commit

---
+++ src/main/kotlin/com/rigvedpatki/kotlinmongodemo/KotlinMongoDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.rigvedpatki.kotlinmongodemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinMongoDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinMongoDemoApplication>(*args)
+}
